07e0ec50ef6291cfc537819ac7226a913436dce9
AdamG682 <89403614+AdamG682@users.noreply.github.com>
2022-03-25 23:25:49 -0600

    comments for card game and js updates

--- sabaccGame.js
+++ sabaccGame.js
@@ -1,236 +1,231 @@
+//Javascript Sabacc (Blackjack) Card Game JS (Adam)
+
+//variables used for the game
+
+	//game and card variables
+	var gameStart = document.getElementById('game-begin');
+	var betButton = document.getElementById('bettings');
+	var buttonOptions = document.getElementById('buttonSelection');
+	var hitButton = document.getElementById("hit-button");
+	var stayButton = document.getElementById("stay-button");
+	var finalResult = document.getElementById("final-result");
+	var resultText = document.getElementById("result-text");
+	var playAgainButton = document.getElementById("againButton");
+	var cardCount = document.getElementById("numberCounter");
+	var cardTypes = ['-8', '+7', '-9', '+10'];
+	var cardNumbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	var game = [];
+	var count = 0;
+
+	//player variables
+	var playerName = document.getElementById("player-name");
+	var betAmount = 0;
+	var playerResult = document.getElementById("playerTally");
+	var playerCards = document.getElementById("player-cards");
+	var playerCount = 0;
+	var playerBalance = document.getElementById("player-balance");
+	var playerEarnings = 1000;
+
+	//rebo (computer player) variables
+	var reboEarnings = document.getElementById("reboTally");
+	var reboCards = document.getElementById("rebo-cards");
+	var reboResult = document.getElementById("reboTally");
+	var reboEarnings = 0;
 
 
-var playerName = document.getElementById("player-name");
-var janelaInicial = document.getElementById('game-begin');
-var apostaButton = document.getElementById('fazerAposta');
-var continuaAposta = document.getElementById('continua-aposta');
-var statsDiv = document.getElementById("stats");
 
-var hitButton = document.getElementById("hit-button");
-var stayButton = document.getElementById("stay-button");
-var finalResult = document.getElementById("final-result");
-var resultText = document.getElementById("result-text");
-var continueButton = document.getElementById("continue-button");
-
-var valorAposta = 0;
-var playerResult = document.getElementById("player-resultado");
-var cartasPlayer = document.getElementById("cartas-player");
-var playerSoma = 0;
-var saldoPlayerDIV = document.getElementById("enteredName");
-var saldoPlayer = 1000;
-
-var compSoma = document.getElementById("comp-resultado");
-var cartasComp = document.getElementById("cartas-comp");
-var compResult = document.getElementById("comp-resultado");
-var compSoma = 0;
-
-var nipe = ['hearts','clubs','diamonds','spades'];
-var tipo = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
-var baralho = [];
-var count=0;
-
-
-function carta(nipe, tipo, valor){
-	this.nipe = nipe;
-	this.tipo = tipo;
-	this.valor = valor;
+//custom function that is able to track the cards that are drawn by value	
+function cardFun(cardTypes, cardNumbers, value) {
+	this.cardTypes = cardTypes;
+	this.cardNumbers = cardNumbers;
+	this.value = value;
 }
 
 
-function startBaralho(){
-	baralho = [];	
+//begins the game with two set of cards
+function startgame() {
+	game = [];
 	var temp;
-	for(var i = 0; i < nipe.length; i++){
-		for(var j = 0; j < tipo.length; j++){
-			
-			if(j > 9 ){
-				temp = new carta(nipe[i], tipo[j], 10);
-			
-			} else{
-				temp = new carta(nipe[i], tipo[j], j + 1);
-			}
+	for (var i = 0; i < cardTypes.length; i++) {
+		for (var j = 0; j < cardNumbers.length; j++) {
+			if (j > 9) {
+				temp = new cardFun(cardTypes[i], cardNumbers[j], 10);
 
-			baralho.push(temp);
+			} else {
+				temp = new cardFun(cardTypes[i], cardNumbers[j], j + 1);
+			}
+			game.push(temp);
 		}
 	}
 }
 
 
+//collects the name that the user enters in the top textbox, will provide an error if the user enters nothing
 nameButton.addEventListener('click', function (event) {
-
-	if(document.getElementById('getPlayer-name').value != ""){
+	if (document.getElementById('getPlayer-name').value != "") {
 
 		var playerName = document.getElementById("player-name");
-		startBaralho();
+		startgame();
 
-	    playerName.innerHTML = "You:"+document.getElementById('getPlayer-name').value;        
-	    saldoPlayerDIV.innerHTML = "<strong>Credit Balance:</strong>"+ saldoPlayer;
-	    janelaInicial.style.visibility = "hidden";
-    }else{
-    	alert("Please enter a name if you want to play traveler.");
-    }
-
+		playerName.innerHTML = "You:" + document.getElementById('getPlayer-name').value;
+		playerBalance.innerHTML = "<strong>Credit Balance:</strong>" + playerEarnings;
+		gameStart.style.visibility = "hidden";
+	} else {
+		alert("Please enter a name if you want to play traveler.");
+	}
 });
 
 
-
-apostaButton.addEventListener('click',function(event){
-
-    if(saldoPlayer > 0){
-    	valorAposta = document.getElementById('valorAposta').value;
-    	iniciaJogada(valorAposta);	
-    }else{
-    	alert("Come back when you have enough credits.");
-    }
-    
-
-    
+//tracks the amount that the user is betting from the dropdown menu, will provide an error if the user has 0 or negative amount when starting a new game
+betButton.addEventListener('click', function (event) {
+	if (playerEarnings > 0) {
+		betAmount = document.getElementById('betAmount').value;
+		startGame(betAmount);
+	} else {
+		alert("Come back when you have enough credits.");
+	}
 });
 
 
-function iniciaJogada (valorAposta) {
-	statsDiv.style.display = "inherit";	
-	var x1 = puxaCarta("player");
-	var x2 = puxaCarta("player");
-	
-	checkBlackjack(x1,x2);
-	aposta.style.display = 'none';
-    continuaAposta.style.display = 'inherit';
+//function draws two cards when the set bet is made
+function startGame(betAmount) {
+	cardCount.style.display = "inherit";
+	var x1 = drawFun("player");
+	var x2 = drawFun("player");
 
+	checkBlackjack(x1, x2);
+	betDropDown.style.display = 'none';
+	buttonOptions.style.display = 'inherit';
 }
-	
 
-function checkBlackjack(x1,x2){
 
-	if(x1 =="A"){
-		if(x2 == "10" || x2 == "J" || x2 == "Q" || x2 == "K"){			
-			setSaldo(valorAposta,"blackjack");
+//function checks to see if the card total reaches 21 on first draw which means that the player automatically wins
+function checkBlackjack(x1, x2) {
+	if (x1 == "A") {
+		if (x2 == "10" || x2 == "J" || x2 == "Q" || x2 == "K") {
+			balance(betAmount, "blackjack");
 		}
 	}
-	else if(x2 =="A"){
-		if(x1 == "10" || x1 == "J" || x1 == "Q" || x1 == "K"){
-			setSaldo(valorAposta,"blackjack");
+	else if (x2 == "A") {
+		if (x1 == "10" || x1 == "J" || x1 == "Q" || x1 == "K") {
+			balance(betAmount, "blackjack");
 		}
 	}
 }
 
-function setIcon(suit){
 
-	if(suit =="clubs"){
+//the four different types of cards that are randomly selected for the game
+function setIcon(typecard) {
+	if (typecard == "-8") {
 		return 'Images/SabaccCard3.png';
 	}
-	else if(suit =="hearts"){
+	else if (typecard == "+7") {
 		return 'Images/SabaccCard4.png';
 	}
-	else if(suit =="diamonds"){
+	else if (typecard == "-9") {
 		return 'Images/SabaccCard5.png';
 	}
-	else if(suit =="spades"){
+	else if (typecard == "+10") {
 		return 'Images/SabaccCard6.png';
 	}
-
 }
 
-function puxaCarta(jogador){
-	
+
+//function that randomly draws a new card for the player and also randomly draws cards for the computer
+function drawFun(user) {
+
 	var x = Math.floor(Math.random() * 52);
-	var cartaTemp = baralho[x];
-	
+	var sabaccCard = game[x];
+	var cardTypesTemp = setIcon(sabaccCard.cardTypes);
 
-	var nipeTemp = setIcon(cartaTemp.nipe);
+	if (user == "player") {
+		var userCardTable = playerCards.innerHTML;
 
-	if(jogador == "player"){
-		var preCartas = cartasPlayer.innerHTML;
+		playerCards.innerHTML = userCardTable + '<div class="carta"><h4 class="carta-cardNumbers">' + sabaccCard.cardNumbers + '</h4><img class="carta-imagem" src="' + cardTypesTemp + '"></div>';
 
-		cartasPlayer.innerHTML = preCartas + '<div class="carta"><h4 class="carta-tipo">'+cartaTemp.tipo+'</h4><img class="carta-imagem" src="'+nipeTemp+'"></div>';			
-		
-		var preSoma = playerResult.innerHTML;
-		playerSoma = playerSoma + cartaTemp.valor;
-		playerResult.innerHTML = 'You:' +playerSoma;
-
+		var userStartingTotal = playerResult.innerHTML;
+		playerCount = playerCount + sabaccCard.value;
+		playerResult.innerHTML = 'You:' + playerCount;
 	}
-	else{
-		var preCartas = cartasComp.innerHTML;
-		cartasComp.innerHTML = preCartas + '<div class="carta"><h4 class="carta-tipo">'+cartaTemp.tipo+'</h4><img class="carta-imagem" src="'+nipeTemp+'"></div>';			
+	else {
+		var reboCardTable = reboCards.innerHTML;
+		reboCards.innerHTML = reboCardTable + '<div class="carta"><h4 class="carta-cardNumbers">' + sabaccCard.cardNumbers + '</h4><img class="carta-imagem" src="' + cardTypesTemp + '"></div>';
 
-		var preSoma = compSoma.innerHTML;
-		compSoma = compSoma + cartaTemp.valor;
-		compResult.innerHTML = 'Max Rebo:' + compSoma;
+		var reboStartingTotal = reboEarnings.innerHTML;
+		reboEarnings = reboEarnings + sabaccCard.value;
+		reboResult.innerHTML = 'Max Rebo:' + reboEarnings;
 	}
 
-	return cartaTemp.tipo;
+	return sabaccCard.cardNumbers;
 }
 
 
-
+//when the hit button is clicked by the player it will check to see if they go over 21, if they do then they automatically lose
 hitButton.addEventListener('click', function (event) {
-
-    	puxaCarta("player");    	
-    	if(playerSoma > 21){
-    
-    		setSaldo(valorAposta,"perdeu");
-    		
-    	}
-    });
+	drawFun("player");
+	if (playerCount > 21) {
+		balance(betAmount, "loser");
+	}
+});
 
 
+//once stay button is clicked, rebo(computer) has their cards revealed to see if the player has won or not
 stayButton.addEventListener('click', function (event) {
-	ComputerPlay();
+	reboPlayerActions();
+});
 
-});	
 
-function ComputerPlay () {	
-	puxaCarta("comp");
-	puxaCarta("comp");
-	
-	while(compSoma < playerSoma){
-		puxaCarta("comp");
+//function that randomly draws cards for rebo and also checks to see if there cards went over 21 and also compares against the player's cards on who has the highest value below 21
+function reboPlayerActions() {
+	drawFun("rebo");
+	drawFun("rebo");
 
-		if(compSoma >21){
-		
-			setSaldo(valorAposta,"ganhou");
+	while (reboEarnings < playerCount) {
+		drawFun("rebo");
+
+		if (reboEarnings > 21) {
+			balance(betAmount, "winner");
 		}
 	}
-	if(compSoma >= playerSoma && compSoma <= 21){
-		
-		setSaldo(valorAposta,"perdeu");
+	if (reboEarnings >= playerCount && reboEarnings <= 21) {
+		balance(betAmount, "loser");
 	}
 }
 
-function setSaldo(valorAposta,estado){
-		hitButton.disabled = true;
-    stayButton.disabled = true;
+
+//function that will either double the winnings or take away the bet amount from the player's balance based on card state
+function balance(betAmount, cardState) {
+	hitButton.disabled = true;
+	stayButton.disabled = true;
 	finalResult.style.display = "inherit";
-	
-	if(estado == "ganhou"){
-		saldoPlayer += valorAposta*2;
-		saldoPlayerDIV.innerHTML = "<strong>Credit Balance:</strong>"+ saldoPlayer;
-		resultText.innerHTML = "YOU WON";	
-	}else if(estado == "blackjack"){
-		saldoPlayer += valorAposta*2;
-		saldoPlayerDIV.innerHTML = "<strong>Credit Balance:</strong>"+ saldoPlayer;
-		resultText.innerHTML = "SABACC!";	
+
+	if (cardState == "winner") {
+		playerEarnings += betAmount * 2;
+		playerBalance.innerHTML = "<strong>Credit Balance:</strong>" + playerEarnings;
+		resultText.innerHTML = "YOU WON";
+	} else if (cardState == "blackjack") {
+		playerEarnings += betAmount * 2;
+		playerBalance.innerHTML = "<strong>Credit Balance:</strong>" + playerEarnings;
+		resultText.innerHTML = "SABACC!";
 	}
-	else{
-		saldoPlayer -= valorAposta;		
-		saldoPlayerDIV.innerHTML = "<strong>Credit Balance:</strong>"+ saldoPlayer;	
+	else {
+		playerEarnings -= betAmount;
+		playerBalance.innerHTML = "<strong>Credit Balance:</strong>" + playerEarnings;
 		resultText.innerHTML = "YOU LOSE";
 	}
 
-	   continueButton.addEventListener('click', function (event) {
-	   		hitButton.disabled = false;
-    		stayButton.disabled = false;
-		   	finalResult.style.display = "none";
-		   	aposta.style.display = 'inherit';
-	    	continuaAposta.style.display = 'none';
-		   	playerSoma = 0;
-		   	compSoma = 0;
-		   	playerResult.innerHTML = 'You:';
-		   	compResult.innerHTML = 'Max Rebo:';
-		   	statsDiv.style.display = "none";
-		   	cartasPlayer.innerHTML = '<h4>You</h4>';
-		   	cartasComp.innerHTML = '<h4>Max Rebo</h4>';
-
-    });
-	
+	playAgainButton.addEventListener('click', function (event) {
+		hitButton.disabled = false;
+		stayButton.disabled = false;
+		finalResult.style.display = "none";
+		betDropDown.style.display = 'inherit';
+		buttonOptions.style.display = 'none';
+		playerCount = 0;
+		reboEarnings = 0;
+		playerResult.innerHTML = 'You:';
+		reboResult.innerHTML = 'Max Rebo:';
+		cardCount.style.display = "none";
+		playerCards.innerHTML = '<h4>You</h4>';
+		reboCards.innerHTML = '<h4>Max Rebo</h4>';
+	});
 }
