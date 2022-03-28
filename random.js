281a5dc0a6374790c68eeab85996a13a7d914ddc
BerxtonChapsticks <89743506+BerxtonChapsticks@users.noreply.github.com>
2022-03-28 16:29:06 -0600

    e

---
+++ random.js
@@ -0,0 +1,58 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const getRandNumBtn = document.getElementById('btn');
+  getRandNumBtn.addEventListener('click', getRandNum);
+});
+
+function getRandNum() {
+  const min = parseInt(document.getElementById('min').value);
+  const max = parseInt(document.getElementById('max').value);
+  if (min && max) {
+      const randNum = Math.floor(Math.random() * (max - min) + min);
+      document.getElementById('output').innerHTML = "<h2 class='col-12'>" + randNum.toString() + " Space Jesus</h2>";
+      let obi = '<br /> <img class= width="100" height="200" src="https://media.vanityfair.com/photos/5d56eac902bf930008778de7/3:2/w_1998,h_1332,c_limit/obi-wan-ewan-series.jpg" />';
+
+      for (let i = 0; i < randNum; i++) {
+          document.getElementById('output').innerHTML += obi;
+      }
+  }
+
+}
+
+var answers = ["It is certain", 
+                   "It is decidedly so", 
+                   "Without a doubt", 
+                   "Yes definitely",
+                   "You may rely on it", 
+                   "As I see it, yes", 
+                   "Most likely", 
+                   "Outlook good", 
+                   "Yes", 
+                   "Signs point to yes",
+                   "Don't count on it", 
+                   "My reply is no",
+                   "My sources say no", 
+                   "Outlook not so good",
+                   "Very doubtful", 
+                   "Reply hazy, try again", 
+                   "Ask again later", 
+                   "Better not tell you now",
+                   "Cannot predict now", 
+                   "Concentrate and ask again"];
+    
+    window.onload = function() {
+       var eight = document.getElementById("eight");
+       var answer = document.getElementById("answer");
+       var ball = document.getElementById("eightball");
+       var question = document.getElementById("question");
+       
+       ball.addEventListener("click", function() {
+         if (question.value.length < 1) {
+           alert('Enter a question!!!');
+         } else {
+           eight.innerText = "";
+           var num = Math.floor(Math.random() * Math.floor(answers.length));
+           answer.innerText = answers[num];
+         }
+       });
+    };
